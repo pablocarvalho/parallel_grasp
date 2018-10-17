1d13fe00b3fa83df91d014d4904cf4dac6d0f34b
pablocarvalho <pablomccarvalho@gmail.com>
2018-10-17 02:32:59 -0300

    adição da classe abstrata que encapsula a criação da thread.   separação da busca local em classe separada do grasp (parametrosBuscaLocal)

--- parallel_Thread_2-mbs-grasp/parametrosBuscaLocal.h
+++ parallel_Thread_2-mbs-grasp/parametrosBuscaLocal.h
@@ -1,6 +1,14 @@
-#include "array.cpp"
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
 
-class ParametrosBuscaLocal
+// --------------------------------------------------------------------------
+#include "abstractThreadClass.h"
+#include "array.cpp"
+#include "matriz.cpp" 
+#include "structs.h"
+
+class ParametrosBuscaLocal : public AbstractThreadClass
 {
 public:	
 	Array<int> A;
@@ -17,6 +25,8 @@ public:
 	int TIMEMAX;
 	int TEST;
 	int* sol;
+
+	  
 
 	ParametrosBuscaLocal(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, Array<int> &b_A, Array<int> &b_B, int ITMAX, int TIMEMAX, int TEST,int* sol)
 	{
@@ -37,7 +47,33 @@ public:
 
 	};
 
+private:
 
+	Matriz<str_e> g;    // grafo booleano de arestas positivas e negativas
+	Array<int>    stat;
+	int           n;             // numero de vertices
+
+
+	bool can_move_C_X_v2(int l, Array<int> &x, Array<int> &y, Array<int> &C);
+  bool can_2move_C_X_v2(int l1, int l2, Array<int> &x, Array<int> &y, Array<int> &C);
+  int  can_move_C_AB_v2(int l, Array<int> &a, Array<int> &b, Array<int> &C, int TEST);
+  int  can_2move_C_AB_v2(int l1, int l2, int num_candA, int num_candB, Array<int> &a, Array<int> &b, Array<int> &C, Array<int> &c, int TEST);
+  bool move_2_C_AB_v2(int desceu, int origem, Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C , Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
+  bool move_3_C_AB_v2(int desceu1, int desceu2, int origem1, int origem2, Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
+
+  void maximiza_solucao_grasp_sig_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C,  Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
+
+	void* busca_local(void* args);
+	int  viz_down_A_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g);
+	int  viz_down_B_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g);
+	int  viz_2down_A_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g, int TIMEMAX);
+	int  viz_2down_B_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g, int TIMEMAX);
+	int  viz_2down_AB_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g, int TIMEMAX);
+	int  obj_grasp_sig_v2(Array<int> &A, Array<int> &B);
+	void testa_particoes_grasp_sig_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c);
+	void atualiza_a_b_c(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c);
+
+	double calcula_tempo(const unsigned long int ini, const unsigned long int fim);
 
 
 };
--- parallel_Thread_2-mbs-grasp/sig_graph.h
+++ parallel_Thread_2-mbs-grasp/sig_graph.h
@@ -67,22 +67,31 @@ public:
   void testa_particoes_grasp_sig_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c);
   void atualiza_a_b_c(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c);
   int  metodo_construtivo_grasp_sig_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
-  void maximiza_solucao_grasp_sig_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C,  Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
+  //void maximiza_solucao_grasp_sig_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C,  Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
+  
+  /*
   bool can_move_C_X_v2(int l, Array<int> &x, Array<int> &y, Array<int> &C);
   bool can_2move_C_X_v2(int l1, int l2, Array<int> &x, Array<int> &y, Array<int> &C);
   int  can_move_C_AB_v2(int l, Array<int> &a, Array<int> &b, Array<int> &C, int TEST);
   int  can_2move_C_AB_v2(int l1, int l2, int num_candA, int num_candB, Array<int> &a, Array<int> &b, Array<int> &C, Array<int> &c, int TEST);
   bool move_2_C_AB_v2(int desceu, int origem, Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C , Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
   bool move_3_C_AB_v2(int desceu1, int desceu2, int origem1, int origem2, Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST);
+  
+  */
+  
+  /*
   int  viz_down_A_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g);
   int  viz_down_B_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g);
   int  viz_2down_A_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g, int TIMEMAX);
   int  viz_2down_B_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g, int TIMEMAX);
   int  viz_2down_AB_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, int TEST, unsigned long int t_ini_g, int TIMEMAX);
   int  obj_grasp_sig_v2(Array<int> &A, Array<int> &B);
+  */
+  
   double calcula_tempo(const unsigned long int ini, const unsigned long int fim);
+  
   int  grasp_sig_v2(Array<int> &A, Array<int> &a, Array<int> &B, Array<int> &b, Array<int> &C, Array<int> &c, Array<int> &cand1, Array<int> &cand2, Array<int> &b_A, Array<int> &b_B, int ITMAX, int TIMEMAX, int TEST);
-  void* busca_local(void* args);
+  //void* busca_local(void* args);
   
   /* ------- TAD ------ */
   void write_sol(const char * name, Array<int> &A, Array<int> &B);
